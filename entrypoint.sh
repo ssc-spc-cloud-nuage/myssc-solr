ce5096e55fcea618fed322a62084c003c4f9b9b1
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2021-12-16 07:35:10 -0500

    Updates9

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,6 @@ if [[ ! -f "${WORKDIR}"/solr.xml ]]; then
     sudo ln -s /opt/docker-solr/solr.xml "${WORKDIR}"/solr.xml
 fi
 
-sudo init_volumes
-
 if [[ -f /opt/solr/bin/solr.in.sh ]]; then
     conf_file=/opt/solr/bin/solr.in.sh
 else
